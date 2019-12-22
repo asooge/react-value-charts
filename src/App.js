bf28d3b0de9010e6300f959b6c52c5832578f61f
asooge <asooge@gmail.com>
2019-12-22 17:56:01 -0500

    displaying accurate chart with hard-coded values for revenue, gross profit, and net income

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Ticker from './Ticker'
+import { Bar } from 'react-chartjs-2'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      financials: {}
+      financials: {},
+      show: null
     }
   }
   render() {
@@ -14,6 +16,34 @@ class App extends React.Component {
       <div>
         <h1>Value Charts</h1>
         <Ticker setApp={this.setState.bind(this)} financials={this.state.financials}/>
+        <Bar
+          data={{
+            labels: [1,2,3,4],
+            datasets: [{
+              label: 'net income',
+              data: [1,2,3,4],
+              backgroundColor: 'black',
+            }, {
+              label: 'gross profit',
+              data: [2,4,6,8],
+              backgroundColor: 'tan',
+            }, {
+              label: 'revenue',
+              data: [10, 11, 20, 22],
+              backgroundColor: 'green'
+            }]
+          }}
+          options = {{
+            scales: {
+              xAxes: [{ stacked: true }],
+              yAxes: [
+                {
+                  stacked: false,
+                  ticks: { beginAtZero: true }
+                }
+              ]}
+          }}
+        />
       </div>
     )
   }
